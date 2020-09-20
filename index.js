ac6d02c7bc5fa9d5b904fafa1e3f3bd0f222816b
Blisston kirubha <blisston.kirubha@inetconnexion.com>
2020-09-20 12:44:12 +0530

    changed black theme

---
+++ index.js
@@ -0,0 +1,53 @@
+
+
+const handleFirstTab = (e) => {
+  if (e.key === 'Tab') {
+    document.body.classList.add('user-is-tabbing')
+
+    window.removeEventListener('keydown', handleFirstTab)
+    window.addEventListener('mousedown', handleMouseDownOnce)
+  }
+
+}
+
+const handleMouseDownOnce = () => {
+  document.body.classList.remove('user-is-tabbing')
+
+  window.removeEventListener('mousedown', handleMouseDownOnce)
+  window.addEventListener('keydown', handleFirstTab)
+}
+
+window.addEventListener('keydown', handleFirstTab)
+
+const backToTopButton = document.querySelector(".back-to-top");
+let isBackToTopRendered = false;
+
+let alterStyles = (isBackToTopRendered) => {
+  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+  backToTopButton.style.transform = isBackToTopRendered
+    ? "scale(1)"
+    : "scale(0)";
+};
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 700) {
+    isBackToTopRendered = true;
+    alterStyles(isBackToTopRendered);
+  } else {
+    isBackToTopRendered = false;
+    alterStyles(isBackToTopRendered);
+  }
+});
+
+
+
+var typed2 = new Typed('#typed', {
+  strings: ['I am a Full stack developer', 'I am from TamilNadu, India'],
+  typeSpeed: 20,
+  backSpeed: 5,
+  backDelay: 2000,
+  startDelay: 1000,
+  fadeOut: true,
+  loop: true
+});
